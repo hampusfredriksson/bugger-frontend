7589aedc0f1bf86dbbf4e98af4c5214797c56da9
Hampus Fredriksson <hampus.fredriksson@chasacademy.se>
2020-03-10 10:37:52 +0100

    Added a header & button, WIP

---
+++ src/components/shared/Button.js
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+
+const Button = styled.button`
+  cursor: pointer;
+  background: transparent;
+  font-size: 16px;
+  border-radius: 3px;
+  color: ${(props) => (props.primary ? "vilolet" : "palevioletred")};
+  border: 2px solid palevioletred;
+  margin: 0 1em;
+  padding: 0.25 1em;
+  transition: 0.3s all ease-out;
+  outline: none;
+
+  &:hover {
+    background-color: ${(props) =>
+      props.primary ? "violet" : "palevioletred"};
+    color: white;
+  }
+`;
+
+export default Button;
