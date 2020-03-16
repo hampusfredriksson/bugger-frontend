9e891709e58a4758509d1e5ed178db6e9c838f7c
Hampus Fredriksson <hampus.fredriksson@chasacademy.se>
2020-03-16 16:42:26 +0100

    Update globalStyle.js
    leaner background

--- src/components/shared/theme/globalStyle.js
+++ src/components/shared/theme/globalStyle.js
@@ -3,6 +3,8 @@ import { createGlobalStyle } from 'styled-components'
 const GlobalStyle = createGlobalStyle`
 body {
   margin: 0;
+  padding: 0; 
+  background-color: #fafafa;
   font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
     'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
     sans-serif;
