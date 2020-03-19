bfe33387ecf99749cafe9f12021778fca4bab68b
Hampus Fredriksson <hampus.fredriksson@chasacademy.se>
2020-03-19 11:45:27 +0100

    add: HoC for authentication

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from "react";
+import React from "react";
 import styled from "styled-components";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import GlobalStyle from "../Shared/Theme/GlobalStyle";
@@ -14,37 +14,36 @@ import AccountPage from "../Account/Account";
 import AdminPage from "../Admin/Admin";
 
 import * as ROUTES from "../../constants/routes";
+import { withAuthentication } from "../Session";
 
 const AppWrapper = styled.div`
   width: 100%;
   margin: auto;
 `;
 
-const App = () => {
-  return (
-    <Fragment>
-      <GlobalStyle />
-      <AppWrapper>
-        <BrowserRouter>
-          <Header />
-          <Navigation />
-          <Switch>
-            <Route exact path={ROUTES.LANDING} component={LandingPage} />
-            <Route exact path={ROUTES.SIGN_IN} component={SignInPage} />
-            <Route exact path={ROUTES.SIGN_UP} component={SignUpPage} />
-            <Route
-              exact
-              path={ROUTES.PASSWORD_FORGET}
-              component={PasswordForgetPage}
-            />
-            <Route exact path={ROUTES.HOME} component={HomePage} />
-            <Route exact path={ROUTES.ACCOUNT} component={AccountPage} />
-            <Route exact path={ROUTES.ADMIN} component={AdminPage} />
-          </Switch>
-        </BrowserRouter>
-      </AppWrapper>
-    </Fragment>
-  );
-};
+const App = () => (
+  <>
+    <GlobalStyle />
+    <AppWrapper>
+      <BrowserRouter>
+        <Header />
+        <Navigation />
+        <Switch>
+          <Route exact path={ROUTES.LANDING} component={LandingPage} />
+          <Route exact path={ROUTES.SIGN_IN} component={SignInPage} />
+          <Route exact path={ROUTES.SIGN_UP} component={SignUpPage} />
+          <Route
+            exact
+            path={ROUTES.PASSWORD_FORGET}
+            component={PasswordForgetPage}
+          />
+          <Route exact path={ROUTES.HOME} component={HomePage} />
+          <Route exact path={ROUTES.ACCOUNT} component={AccountPage} />
+          <Route exact path={ROUTES.ADMIN} component={AdminPage} />
+        </Switch>
+      </BrowserRouter>
+    </AppWrapper>
+  </>
+);
 
-export default App;
+export default withAuthentication(App);
