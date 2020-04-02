c3fcabbe4770deb01db867fa639e03731c23610b
Hampus Fredriksson <hampus.fredriksson@chasacademy.se>
2020-04-02 15:50:06 +0200

    add: styled signin page

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -1,17 +1,58 @@
 import React, { Component } from "react";
 import { withRouter } from "react-router-dom";
 import { compose } from "recompose";
-
+import styled from "styled-components";
 import { SignUpLink } from "../SignUp/SignUp";
 import { withFirebase } from "../Firebase";
 
 import * as ROUTES from "../../constants/routes";
 
+const Form = styled.form`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 25rem;
+  padding: 2.5rem;
+  box-sizing: border-box;
+  background: rgba(0, 0, 0, 0.3);
+  color: white;
+  border-radius: 0.625rem;
+`;
+
+const Button = styled.button`
+  outline: 0;
+  text-transform: uppercase;
+  background-color: #434c5e;
+  width: 100%;
+  border: 0;
+  padding: 15px;
+  color: #ffffff;
+  font-size: 14px;
+  -webkit-transition: all 0.3 ease;
+  transition: all 0.3 ease;
+  cursor: pointer;
+  &:hover {
+    background-color: #6d7c9c;
+  }
+`;
+
+const Input = styled.input`
+  width: 100%;
+  padding: 0.625rem 0;
+  font-size: 1.1rem;
+  color: #fff;
+  letter-spacing: 0.062rem;
+  margin-bottom: 1.875rem;
+  border: none;
+  border-bottom: 0.065rem solid #fff;
+  outline: none;
+  background: transparent;
+`;
+
 const SignInPage = () => (
   <div>
-    <h1>Sign In</h1>
     <SignInForm />
-    <SignUpLink />
   </div>
 );
 
@@ -53,27 +94,31 @@ class SignInFormBase extends Component {
     const isInvalid = password === "" || email === "";
 
     return (
-      <form onSubmit={this.onSubmit}>
-        <input
-          name="email"
-          value={email}
-          onChange={this.onChange}
-          type="text"
-          placeholder="email"
-        />
-        <input
-          name="password"
-          value={password}
-          onChange={this.onChange}
-          type="password"
-          placeholder="password"
-        />
-        <button disabled={isInvalid} type="submit">
-          Sign In
-        </button>
+      <>
+        <Form onSubmit={this.onSubmit}>
+          <h2>Login</h2>
+          <Input
+            name="email"
+            value={email}
+            onChange={this.onChange}
+            type="text"
+            placeholder="email"
+          />
+          <Input
+            name="password"
+            value={password}
+            onChange={this.onChange}
+            type="password"
+            placeholder="password"
+          />
+          <Button disabled={isInvalid} type="submit">
+            Sign in
+          </Button>
 
+          <SignUpLink />
+        </Form>
         {error && <p>{error.message}</p>}
-      </form>
+      </>
     );
   }
 }
