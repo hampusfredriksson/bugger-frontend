d64433777171e74b65cc3a1d692ef7707801e509
Hampus Fredriksson <hampus.fredriksson@chasacademy.se>
2020-04-02 15:49:36 +0200

    removed unused code

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,11 +4,11 @@ import { Link } from "react-router-dom";
 import Logo from "../images/bugger.png";
 import Button from "./Styles/Button";
 
-const Nav = styled.div` 
-  box-shadow: 0 4px 5px rgba(0, 0, 0, 0.50);
+const Nav = styled.div`
+  box-shadow: 0 4px 5px rgba(0, 0, 0, 0.5);
 `;
 const NavHeader = styled.div`
-padding: 3px;
+  padding: 3px;
   width: 98%;
   display: flex;
   align-items: center;
@@ -22,24 +22,7 @@ const NavCenter = styled.div`
   width: 33.333%;
   text-align: center;
 `;
-const Input = styled.input`
-  font-size: 16px;
-  border: solid 1px #dbdbdb;
-  border-radius: 3px;
-  color: #262626;
-  padding: 7px 33px;
-  border-radius: 3px;
-  color: #999;
-  cursor: text;
-  font-size: 14px;
-  font-weight: 300;
-  text-align: center;
-  background: #fafafa;
-  &:active,
-  &:focus {
-    text-align: left;
-  }
-`;
+
 const NavRight = styled.div`
   width: 33.333%;
   text-align: right;
@@ -59,22 +42,20 @@ const LogoImage = styled.img`
 const Header = () => {
   return (
     <Nav>
-        <NavHeader>
-          <NavLeft>
-            <Link to="/">
-              <LogoImage src={Logo} />
-            </Link>
-          </NavLeft>
-          <NavCenter>
-            <Button>Another test</Button>
-            <Input type="text" placeholder="Search" />
-          </NavCenter>
-          <NavRight>
-            <Link to="/login">
-              <ProfileImage src="https://api.adorable.io/avatars/285/abott@adorable.png" />
-            </Link>
-          </NavRight>
-        </NavHeader>
+      <NavHeader>
+        <NavLeft>
+          <Link to="/">
+            <LogoImage src={Logo} />
+          </Link>
+        </NavLeft>
+        <NavCenter>
+        </NavCenter>
+        <NavRight>
+          <Link to="/login">
+            <ProfileImage src="https://api.adorable.io/avatars/285/abott@adorable.png" />
+          </Link>
+        </NavRight>
+      </NavHeader>
     </Nav>
   );
 };
