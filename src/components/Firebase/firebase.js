63c4c9b4d4906afcd64899e26c5abb7983cbe524
Hampus Fredriksson <hampus.fredriksson@chasacademy.se>
2020-05-03 21:49:37 +0200

    fixed db config and updated env

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -1,37 +1,37 @@
-import app from "firebase/app";
+import * as firebase from "firebase/app";
 import "firebase/auth";
 import "firebase/firestore";
 
 const config = {
-  apiKey: "AIzaSyC1_WgmzqpH52WJMwKLA9XCqgDxDHagY7k",
-  authDomain: "bugger-d1c9b.firebaseapp.com",
-  databaseURL: "https://bugger-d1c9b.firebaseio.com",
-  projectId: "bugger-d1c9b",
-  storageBucket: "bugger-d1c9b.appspot.com",
-  messagingSenderId: "646721874617",
-  appId: "1:646721874617:web:2282f32c86d08631f00968",
-  measurementId: "G-2T15742S3N",
+  // apiKey: "AIzaSyC1_WgmzqpH52WJMwKLA9XCqgDxDHagY7k",
+  // authDomain: "bugger-d1c9b.firebaseapp.com",
+  // databaseURL: "https://bugger-d1c9b.firebaseio.com",
+  // projectId: "bugger-d1c9b",
+  // storageBucket: "bugger-d1c9b.appspot.com",
+  // messagingSenderId: "646721874617",
+  // appId: "1:646721874617:web:2282f32c86d08631f00968",
+  // measurementId: "G-2T15742S3N",
 
   // This fucker wont work...
-  // apiKey: process.env.REACT_APP_API_KEY,
-  // authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  // databaseURL: process.env.REACT_APP_DATABASE_URL,
-  // projectId: process.env.REACT_APP_PROJECT_ID,
-  // storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  // messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  // appId: process.env.REACT_APP_ID,
-  // measurementId: process.env.REACT_APP_MEASUREMENT_ID
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
+firebase.initializeApp(config);
+
+const db = firebase.firestore();
 
 class Firebase {
   constructor() {
-    app.initializeApp(config);
-
     // Helpers
-    this.fieldValue = app.firestore.FieldValue;
-
-    this.auth = app.auth();
-    this.db = app.firestore();
+    this.fieldValue = firebase.FieldValue;
+    this.auth = firebase.auth();
+    this.db = firebase.firestore();
   }
 
   // Auth API
@@ -82,5 +82,5 @@ class Firebase {
   message = (uid) => this.db.doc(`messages/${uid}`);
   messages = () => this.db.collection("messages");
 }
-
+export { db };
 export default Firebase;
