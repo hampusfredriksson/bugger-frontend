33cee512c5186842c20f69d34ef1f404407048ac
Hampus Fredriksson <hampus.fredriksson@chasacademy.se>
2020-05-14 14:24:56 +0200

    add: display bug component

--- src/components/BugDetail.jsx
+++ src/components/BugDetail.jsx
@@ -1,10 +1,25 @@
 import React from "react";
+import styled from "styled-components";
+
+const Bugs = styled.div`
+
+`;
+
+const Title = styled.h3`
+margin: 0;
+`;
+
+const Value = styled.p`
+
+`;
 
 const BugDetail = ({ title, value }) => {
   return (
     <>
-      <h4>{title}</h4>
-      <p>{value}</p>
+      <Bugs>
+          <Title>{title}</Title>
+        <Value>{value}</Value>
+      </Bugs>
     </>
   );
 };
