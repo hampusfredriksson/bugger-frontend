03083b021ccb04fedc6e2583fdb15e19b19df6a6
Hampus Fredriksson <fredrikhampusson@gmail.com>
2020-10-19 23:43:47 +0200

    css tweaks

--- src/components/NotFound/index.js
+++ src/components/NotFound/index.js
@@ -8,15 +8,25 @@ const Container = styled.div`
   padding: 8%;
   margin: auto;
   font-family: "FiraCode-Retina";
+  margin-top: 1rem;
+`;
+
+const Message = styled.div`
+  margin-bottom: 10rem;
 `;
 
 const NotFoundPage = () => {
   return (
     <Container>
+      <Message style={{ textAlign: "center" }}>
+        <h1>Oops!</h1>
+        <p>We can't seem to find the page you're looking for.</p>
+
+        <Link to="/">
+          <u>Home</u>
+        </Link>
+      </Message>
       <img src={PageNotFound} alt="404" />
-      <p style={{ textAlign: "center" }}>
-        <Link to="/">Go to Home </Link>
-      </p>
     </Container>
   );
 };
